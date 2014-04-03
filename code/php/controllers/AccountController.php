e32b742188c1557495302cacffe84b9e8dce045a
Ben <ben.dimock@gmail.com>
2014-04-03 14:38:40 -0400

    making deposit code more robust
    previous code didn’t handle the absence of a user ledger row well.
    while this condition should never occur, better error handling seemed
    prudent.

--- code/php/controllers/AccountController.php
+++ code/php/controllers/AccountController.php
@@ -13,7 +13,6 @@ class AccountController extends Controller
       
       public function accessRules()
       {
-            
             return array(
                   array('allow', 
                         'actions'=>array('deposit','withdraw'),
@@ -30,11 +29,12 @@ class AccountController extends Controller
             $card = $_POST['stripeToken'];
             $amount = $_POST['amount'];
             
-            try {
+            try {            
+                  $activeLedger = UserLedgerController::getActiveRecord(Yii::app()->user->id);                         
                   $record = StripeLiason::getInstance()->charge($card, $amount);
-                  $success = $this->recordDeposit($record);   
-                  Yii::app()->user->setFlash('success', 'Your deposit has been made.');
-                   
+                  $success = $this->recordDeposit($activeLedger, $record);
+                     
+                  Yii::app()->user->setFlash('success', 'Your deposit has been made.'); 
             } catch(Exception $e) {
                   Yii::app()->user->setFlash('error', $e->getMessage());
             }
@@ -43,33 +43,28 @@ class AccountController extends Controller
             $this->redirect(array('user/view'));
       }
         
+
       
       // @todo are we a little uncomfortable with this active row business.... what if somehow there are rows
       // but they all get set to 0, then we're going to re-initialize the account here, erroneously...      
-      private function recordDeposit($chargeArray) 
+      private function recordDeposit($activeLedger, $chargeArray) 
       {
-            $success = false;
-            $uid = Yii::app()->user->id;           
+   
             $amount = $chargeArray['amount'] / 100;   //Stripe transactions are denominated in pennies
             $credits = $chargeArray['fee'] / 100;     //Stripe transactions are denominated in pennies
-            
             $cash = $amount - $credits;
-            
             $ref = 'Stripe[' . $chargeArray['id'] . ']';          
             
-            $activeLedger = UserLedger::model()->findByAttributes(array('user_id'=>$uid,'active'=>1));  
-            $newLedger = $activeLedger->recordTransaction(LedgerTransactions::DEPOSIT,$cash, $ref, $credits);
-    
             $transaction = Yii::app()->db->beginTransaction();                                                  
             
-            try {          
+            try {     
+                  $newLedger = $activeLedger->recordTransaction(LedgerTransactions::DEPOSIT,$cash, $ref, $credits);
                   $this->saveModel($newLedger);
                   
                   if(!empty($activeLedger))
                         $this->saveModel($activeLedger);
                   
                   $transaction->commit();
-                  $success = true;
             } catch(Exception $e) {
                   $transaction->rollback();
                   throw $e;
@@ -78,7 +73,6 @@ class AccountController extends Controller
                   // should:  alert user that there was a problem.  notify admin.  give them the 
                   // stripe reference ID.
             }
-            return $success;
       }
 }
 
--- code/php/controllers/UserLedgerController.php
+++ code/php/controllers/UserLedgerController.php
@@ -5,10 +5,22 @@ class UserLedgerController extends Controller
 	
 	public static function getActiveRecord($userId) 
 	{
-		return UserLedger::model()->findByAttributes(array(
+		$model = UserLedger::model()->findByAttributes(array(
 				'user_id'=>$userId,
-				'active'=>TRUE));		
+				'active'=>TRUE));	
+                        
+            if(empty($model))
+                  throw new Exception('Unable to pull active user ledger record');	
+            
+            return $model;
 	}
+      
+      // @todo To be used when we're upddating the ledger.  prevents race conditions, etc. 
+      // requires a lock field to be added and possibly a process id?  
+      public static function acquireActiveRecord($userId) 
+      {                 	            
+      }
+         
 }
 
 ?>
